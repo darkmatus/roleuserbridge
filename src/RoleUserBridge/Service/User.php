2b79730fd0691df4b5169ce11544bad1b3b819ea
darkmatus <admin@adventure-hack.de>
2014-03-17 09:53:25 +0100

    Update User.php

--- src/RoleUserBridge/Service/User.php
+++ src/RoleUserBridge/Service/User.php
@@ -8,25 +8,6 @@ use RoleUserBridge\Mapper\RoleMapper;
 
 use ZfcUser\Service\User as ZfcUser;
 
-/**
- * @category    Test
- * @package     Test_Model
- * @subpackage  Adapter
- * @copyright   Copyright (c) 18.10.2012 Unister GmbH
- * @author      Michael Müller <michael.mueller@unister.de>
- * @version     $Id:$
- */
-
-/**
- * Kurze Beschreibung der Klasse
- *
- * Lange Beschreibung der Klasse (wenn vorhanden)...
- *
- * @category    Test
- * @package     Test_Model
- * @subpackage  Adapter
- * @copyright   Copyright (c) 18.10.2012 Unister GmbH
- */
 class User extends ZfcUser
 {
 
